0880d743478da27ab6fa3f6da8c3435df3cbbc7c
Cobardi <senhajiilias0@gmail.com>
2026-03-14 15:58:28 +0100

    fix: add alias exports for appointments and chatSessions

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -1,3 +1,9 @@
 export * from "./appointments";
 export * from "./contact_messages";
 export * from "./chat_sessions";
+
+import { appointmentsTable } from "./appointments";
+import { chatSessionsTable } from "./chat_sessions";
+
+export const appointments = appointmentsTable;
+export const chatSessions = chatSessionsTable;
